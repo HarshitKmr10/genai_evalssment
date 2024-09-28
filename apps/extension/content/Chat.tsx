574b5953babbc2ccc847c220d056819d7d2a447d
Chaitanya Giri <chaitanyagiri2@gmail.com>
2024-09-28 16:45:27 +0530

    feat: add chatting to extension

---
+++ apps/extension/content/Chat.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useRef, useState } from "react";
+import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import { cn } from "../lib/utils";
+
+const MAX_TEXTAREA_HEIGHT = 100;
+
+const Chat = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [chatMessages, setChatMessages] = useState<string[]>([]);
+  const [chatLoading, setChatLoading] = useState(false);
+
+  function resetTextareaHeight() {
+    const textarea = textareaRef.current!;
+    textarea.style.height = "auto";
+    textarea.style.height =
+      Math.min(textarea.scrollHeight, MAX_TEXTAREA_HEIGHT) + "px";
+    const maxHeightReached = textarea.scrollHeight >= MAX_TEXTAREA_HEIGHT;
+    textarea.classList.toggle("overflow-y-hidden", !maxHeightReached);
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      if (userCannotSendMessage()) return false;
+      addUserMessage();
+      resetTextareaHeight();
+    }
+  }
+
+  useEffect(() => {
+    if (chatMessages.length === 0) return;
+    document
+      .querySelectorAll(".message")
+      ?.item(chatMessages.length - 1)
+      ?.scrollIntoView({ behavior: "smooth" });
+  }, [chatMessages]);
+
+  function userCannotSendMessage() {
+    return (
+      chatMessages.length % 2 !== 0 ||
+      textareaRef.current!.value === "" ||
+      chatLoading
+    );
+  }
+
+  function handleClick() {
+    if (userCannotSendMessage()) return;
+    addUserMessage();
+  }
+
+  function addUserMessage() {
+    const message = textareaRef.current!.value;
+    setChatMessages((prev) => [...prev, message]);
+    textareaRef.current!.value = "";
+    askQuestion(message);
+  }
+
+  async function askQuestion(query: string) {
+    setChatLoading(true);
+    const response = { data: query };
+    setChatMessages((prev) => [...prev, response.data]);
+    setChatLoading(false);
+  }
+
+  return (
+    <div className="relative flex h-full shrink-0 flex-col gap-4 rounded-lg p-2 shadow">
+      <h3 className="text-center text-lg font-semibold text-gray-300">Chat</h3>
+      <div className="grow">
+        <div className="h-[250px] overflow-auto whitespace-pre-wrap">
+          <div className="mb-16 space-y-2">
+            {chatMessages.map((message, index) => (
+              <div
+                key={index}
+                className={cn(
+                  "message flex w-max max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
+                  index % 2 === 0 ? "ml-auto bg-black/40" : "bg-muted",
+                )}
+              >
+                {message}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 mb-4 w-full px-4">
+        <div className="relative flex w-full rounded-md bg-zinc-800 py-2 shadow-lg md:py-3 md:pl-4">
+          <textarea
+            ref={textareaRef}
+            name="user-message"
+            rows={1}
+            className="max-h-[200px] w-full resize-none overflow-y-hidden border-0 bg-transparent pl-2 pr-7 outline-none md:pl-0"
+            placeholder="Ask Something..."
+            onInput={resetTextareaHeight}
+            onKeyDown={handleKeyDown}
+          ></textarea>
+          <button
+            type="submit"
+            className="absolute bottom-1.5 right-1 p-1 text-lg text-[#91ADF6] md:bottom-2.5 md:right-2"
+            onClick={handleClick}
+          >
+            <PaperPlaneIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- apps/extension/content/ContentApp.tsx
+++ apps/extension/content/ContentApp.tsx
@@ -1,119 +1,51 @@
-import { useEffect, useRef, useState } from "react";
-import reactLogo from "../images/react.png";
-import tailwindBg from "../images/tailwind_bg.png";
-import typescriptLogo from "../images/typescript.png";
-import tailwindLogo from "../images/tailwind.png";
-import chromeWindowBg from "../images/chromeWindow.png";
+import { useState } from "react";
 import Draggable from "react-draggable";
 import { buttonVariants } from "../components/ui/button";
 import { DragHandleDots2Icon } from "@radix-ui/react-icons";
 import { cn } from "../lib/utils";
 import clsx from "clsx";
+import Chat from "./Chat";
 
 export default function ContentApp() {
-  const [isdialogOpen, setIsDialogOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsOpen((prev) => !prev);
   };
 
-  if (!isdialogOpen) {
-    return (
-      <Draggable handle="svg">
-        <div className="relative inline-block">
-          <div
-            className={cn(
-              buttonVariants({ variant: "default", size: "lg" }),
-              "px-0 pr-4 hover:bg-primary",
-              clsx({
-                "rounded-none rounded-t-lg": isOpen,
-                "shadow-lg": !isOpen,
-              }),
-            )}
-          >
-            <div className="mr-2 border-r px-2">
-              <DragHandleDots2Icon className="h-4 w-4 cursor-grab" />
-            </div>
-            <button onClick={toggleDropdown} className="focus:outline-none">
-              DSA Tutor
-            </button>
-          </div>
-
-          {isOpen && (
-            <div
-              className={cn(
-                "absolute left-0 z-50 w-[250px] rounded-none bg-primary p-4 text-primary-foreground shadow-xl",
-                clsx({ "rounded-b-lg rounded-tr-lg": isOpen }),
-              )}
-            >
-              HIII
-            </div>
-          )}
-        </div>
-      </Draggable>
-    );
-  }
-
   return (
-    <div className="mx-auto max-w-7xl md:px-0 lg:p-6">
-      <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:h-[100vh] md:h-full md:pt-24 lg:flex lg:gap-x-20 lg:rounded-3xl lg:px-24 lg:pt-0">
-        <div className="pointer-events-none absolute inset-x-0 top-0 z-20 flex justify-center overflow-hidden">
-          <div className="flex w-[108rem] flex-none justify-end">
-            <picture>
-              <img
-                src={tailwindBg}
-                alt=""
-                className="hidden w-[90rem] max-w-none flex-none dark:block"
-                decoding="async"
-              />
-            </picture>
-          </div>
-        </div>
-        <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-12 lg:text-left">
-          <div className="mx-auto my-4 flex items-center justify-center space-x-4">
-            <img
-              alt="React logo"
-              src={reactLogo}
-              className="relative inline-block w-12"
-            />
-            <div className="text-3xl text-white">+</div>
-            <img
-              alt="TypeScript logo"
-              src={typescriptLogo}
-              className="relative inline-block w-12"
-            />
-            <div className="text-3xl text-white">+</div>
-            <img
-              alt="Tailwind logo"
-              src={tailwindLogo}
-              className="relative inline-block w-12"
+    <Draggable handle="#drag-handle">
+      <div className="relative inline-block">
+        <div
+          className={cn(
+            buttonVariants({ variant: "default", size: "lg" }),
+            "px-0 pr-4 hover:bg-primary",
+            clsx({
+              "rounded-b-none": isOpen,
+              "shadow-lg": !isOpen,
+            }),
+          )}
+        >
+          <div className="px-2">
+            <DragHandleDots2Icon
+              id="drag-handle"
+              className="h-4 w-4 cursor-grab"
             />
           </div>
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            This is a content script running React, TypeScript, and Tailwind.css
-          </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Learn more about creating cross-browser extensions by{" "}
-            <button
-              onClick={() => setIsDialogOpen(false)}
-              className="underline hover:no-underline"
-            >
-              closing this hint
-            </button>
-            .
-          </p>
+          <button onClick={toggleDropdown} className="focus:outline-none">
+            DSA Tutor
+          </button>
         </div>
-        <div className="relative mt-16 h-80 lg:mt-8">
-          <img
-            className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-            src={chromeWindowBg}
-            alt="Chrome window screenshot"
-            width="1824"
-            height="1080"
-          />
+
+        <div
+          className={cn(
+            "absolute left-0 z-50 hidden w-[300px] rounded-md rounded-tl-none bg-primary text-primary-foreground shadow-2xl",
+            clsx({ block: isOpen }),
+          )}
+        >
+          <Chat />
         </div>
       </div>
-    </div>
+    </Draggable>
   );
 }
